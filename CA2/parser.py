a0e7afef329ab03d98a6de112b014a01dae2ee5f
ShayanShabihi <shabihish@gmail.com>
2022-05-01 11:17:59 +0430

    Make final tcl changes

--- CA2/parser.py
+++ CA2/parser.py
@@ -2,14 +2,12 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def get_truncated_tr_lines(tr_lines: list):
     out = list()
     for i, line in enumerate(tr_lines):
         if re.search('^[sr].*-It tcp.*', line):
             out.append(line)
     return out
-
 
 class TrParser:
     def __init__(self, tr_file_path: str, th0_file_path: str, th1_file_path: str):
@@ -49,18 +47,17 @@ class TrParser:
         arr_th1 = np.genfromtxt(self.th1_file_path, dtype=np.float64)
 
         plt.plot(arr_th1[:, 1] + arr_th0[:, 1])
-        plt.ylim([0, 300000])
+        # plt.ylim([0, 300000])
         plt.show()
-        return np.r_[arr_th0, arr_th1][:, 1].mean()
-
+        return (arr_th0[:, 1] + arr_th1[:, 1]).mean()
 
 if __name__ == '__main__':
     parser = TrParser('wireless.tr', 'throughput0.tr', 'throughput1.tr')
 
     print('Now running...\nThis may take a few moments. Please be patient...')
-    # packet_stats = parser.parse_tr_for_packet_statistics()
+    packet_stats = parser.parse_tr_for_packet_statistics()
     avg_throughput = parser.get_avg_throughput()
 
     print('Avg throughput: {} bits/sec'.format(avg_throughput))
-    # print('Avg end-to-end (one-way) delay: {} secs'.format(packet_stats['avg_oneway_delay']))
-    # print('Avg transfer ratio: {}'.format(packet_stats['avg_transfer_ratio']))
+    print('Avg end-to-end (one-way) delay: {} secs'.format(packet_stats['avg_oneway_delay']))
+    print('Avg transfer ratio: {}'.format(packet_stats['avg_transfer_ratio']))
